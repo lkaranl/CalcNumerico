84e070f8d425e63cfa34e134da0285164b280071
Karan Luciano <32453979+lkaranl@users.noreply.github.com>
2020-09-03 13:02:49 -0400

    Update Vandermonde.py

--- Vandermonde.py
+++ Vandermonde.py
@@ -1,11 +1,13 @@
 import numpy as np
 import sympy        
 
+#   EXEMPLOS DE PONTOS
 # pontos = [(x,y), (x,y) ...]
 # pontos = [(1,0),(-1,0),(0,1)]
-# pontos = [(3,20),(5,30),(7,50)]
 # pontos = [(3,120),(5,195),(9,225)]
-pontos = [(1,0),(1.01,2),(2,7),(3,26)]
+#pontos = [(-1,0),(1,2),(2,7),(3,26)]
+pontos = [(3,20),(5,30),(7,50)]
+
 n = len(pontos)
 def vandermond(pontos):
     xs,ys = zip(*pontos)
@@ -14,9 +16,11 @@ def vandermond(pontos):
     a = np.linalg.solve(A,B)
     return a
 
-a = vandermond(pontos)
+def aux_a():
+    return vandermond(pontos)
 
 def p(x):
+    a = aux_a()
     px = sum([a[k] * x ** k for k in range(n)])
     return  px
 
@@ -27,21 +31,22 @@ def sign(x):
 
 def equation(pontos):
     # eq = "p(x) = "
+    a = aux_a()
     eq = "".join([f'{sign(a[k])}*x**{k}' for k in range(n)])
     return eq
 
-print("Método de Vandermonde\n")
-eq = equation(pontos)
+def imprimir():
+    print("Método de Vandermonde\n")
+    eq = equation(pontos)
 
-teste = eq
-aa = sympy.expand(teste)
-print("Polinomio -->",eq)
-print("Polinomio Simplificado --> ",aa)
+    aa = sympy.expand(eq)
+    print("Polinomio -->",eq)
+    print("Polinomio Simplificado --> ",aa)
 
+    px = float(input("Qual ponto deseja? "))
+    ppx = p(px)
+    print("Se P (x) = {} --> {}".format(px,ppx))
 
-px = float(input("Qual ponto deseja? "))
-ppx = p(px)
-print("Se P (x) = {} --> {}".format(px,ppx))
+if __name__ == "__main__":
+    imprimir()
 
-
-        # print("Se P (x) = {} --> {}".format(x,c))
